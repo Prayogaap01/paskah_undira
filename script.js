a70d0a125ef73c6735e1f0dc8706b1fac9401ee5
Prayoga.ap01 <144861020+Prayogaap01@users.noreply.github.com>
2026-03-22 01:04:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ const openFlyer = document.getElementById("openFlyer");
 const modal = document.getElementById("flyerModal");
 const closeModal = document.getElementById("closeModal");
 
+// buka modal
 openFlyer.onclick = () => {
-modal.classList.add("show");
+  modal.classList.add("show");
 }
 
+// tutup modal (klik X)
 closeModal.onclick = () => {
-modal.classList.remove("show");
+  modal.classList.remove("show");
 }
 
+// tutup modal (klik luar gambar)
 modal.onclick = (e) => {
-if(e.target === modal){
-modal.classList.remove("show");
-}
+  if (e.target === modal) {
+    modal.classList.remove("show");
+  }
 }
 
+// tambahan: ESC keyboard
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    modal.classList.remove("show");
+  }
+});
